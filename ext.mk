cb4ca50b520248324ca375ebb38ad1179f1b0d1f
Foez Ahmed <foez.official@gmail.com>
2026-08-11 22:30:03 +0600

    modified:   .gitmodules modified:   ext.mk new file:   submodule/adn_apb new file:   submodule/adn_clk_rst new file:   submodule/adn_uart

--- ext.mk
+++ ext.mk
@@ -1,10 +1,16 @@
 HAS_SUBMODULES = 1
 
-export ADN_TEMPLATE=$(CURDIR)
-export REPO_NAME_EXP=ADN_TEMPLATE
+export APB_UART=$(CURDIR)
+export REPO_NAME_EXP=APB_UART
 
 export ADN_COMMON=$(REPO_ROOT)/submodule/adn_common
+export ADN_APB=$(REPO_ROOT)/submodule/adn_apb
+export ADN_CLK_RST=$(REPO_ROOT)/submodule/adn_clk_rst
+export ADN_UART=$(REPO_ROOT)/submodule/adn_uart
 
 .PHONY: compile_all_submodules
 compile_all_submodules:
 	@make -s compile_submodule SUB=adn_common
+	@make -s compile_submodule SUB=adn_apb
+	@make -s compile_submodule SUB=adn_clk_rst
+	@make -s compile_submodule SUB=adn_uart
