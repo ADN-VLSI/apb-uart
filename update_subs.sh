65b3015a4c6ba804e93b45e54b0c458115a8b5b2
Foez Ahmed <foez.official@gmail.com>
2026-08-12 16:13:35 +0600

    Update submodules to latest main

--- update_subs.sh
+++ update_subs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+git submodule deinit -f --all
+git submodule update --init
 git submodule foreach 'git checkout main && git reset --hard $$(git rev-list --max-parents=0 HEAD) && git pull'
 git add .
 git commit -m "Update submodules to latest main" || echo "No changes to commit"
